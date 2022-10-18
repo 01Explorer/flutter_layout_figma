c33ce9a6c76c409a7e4dbcf1a296b3c5c6e56334
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-18 12:20:24 -0300

    Created the theme for the app and the needed components

---
+++ lib/themes/theme_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const MaterialColor primaryColor = Colors.red;
+  static const List<Color> headerGradient = [
+    Color.fromRGBO(195, 20, 50, 1),
+    Color.fromRGBO(36, 11, 54, 1),
+  ];
+
+  static const Map recenActivity = {
+    'spent': Color.fromRGBO(255, 175, 29, 1),
+    'income': Color.fromRGBO(36, 11, 54, 1),
+  };
+
+  static const Color division = Color.fromRGBO(166, 166, 166, 1);
+}
