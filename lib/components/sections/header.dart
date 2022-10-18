aaae10aadb39b7537d8dba5376e3e4e73fef5af9
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-18 10:29:14 -0300

    Made the basic Header component

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -6,16 +6,56 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Text('\$1000.00'),
-              const Text('Balanço disponível')
-            ],
-          ),
-          const Icon(Icons.account_circle),
-        ],
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color.fromRGBO(103, 99, 234, 1),
+            Color.fromRGBO(155, 105, 254, 1),
+            Color.fromRGBO(195, 107, 255, 1),
+          ],
+        ),
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(10),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 80, 16, 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text.rich(
+                  TextSpan(
+                    text: '\$',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: '1000.00',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28,
+                          )),
+                    ],
+                  ),
+                ),
+                const Text(
+                  'Balanço disponível',
+                  style: TextStyle(fontSize: 16),
+                )
+              ],
+            ),
+            const Icon(
+              Icons.account_circle,
+              size: 42,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Alubank extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Alubank',
       home: Home(),
     );
